d096ff35e1cc1c453de2902200c4fd3537c12c17
HANSEN EMANUEL <hansenemanuel@hotmail.com>
2022-04-18 17:56:40 +0700

    cors issue

--- api/server.js
+++ api/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://comms-app.netlify.app',
+  origin: ['https://comms-app.netlify.app','http;//localhost:3000'],
   credentials: true,
 }));
 
