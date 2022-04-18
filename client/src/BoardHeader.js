7824a234f3b68a32b019b6eafac3251536913938
HANSEN EMANUEL <hansenemanuel@hotmail.com>
2022-04-18 13:57:16 +0700

    Change header

--- client/src/BoardHeader.js
+++ client/src/BoardHeader.js
@@ -1,3 +1,5 @@
+import comms from "./Comms_notext.png";
+
 function BoardHeader() {
   return (
     <>
@@ -6,11 +8,11 @@ function BoardHeader() {
       <div className="bg-reddit_dark-brighter">
         <div className="mx-6 relative flex">
           <div className="h-20 w-20 rounded-full overflow-hidden relative -top-3 border-4 border-white bg-white">
-            <img src="https://styles.redditmedia.com/t5_2qs0q/styles/communityIcon_5ey8lzmwmxp21.png?width=256&s=5a85d5c682f40e3cf317c560b219585ac0afce78" alt=""/>
+            <img src={comms} alt=""/>
           </div>
           <div className="pt-2 pl-4">
-            <h1 className="text-gray-300 text-3xl">webdev: reddit for web developers</h1>
-            <h5 className="text-gray-500">r/webdev</h5>
+            <h1 className="text-gray-300 text-3xl">Comms: Thread for all of us</h1>
+            <h5 className="text-gray-500">r/Comms</h5>
           </div>
         </div>
       </div>
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,4 +1,5 @@
 import Logo from "./logo.png";
+import Comms from "./Comms.png";
 import {
   BellIcon,
   ChatIcon,
@@ -32,14 +33,14 @@ function Header() {
     <header className="w-full bg-reddit_dark p-2">
       <div className="mx-4 flex relative">
         <Link to="/">
-          <img src={Logo} alt="" className="w-8 h-8 mr-4"/>
+          <img src={Comms} alt="" className="h-8 mr-4"/>
         </Link>
-        <form action="" className="bg-reddit_dark-brighter px-3 flex rounded-md border border-reddit_border mx-4 flex-grow">
+        {/* <form action="" className="bg-reddit_dark-brighter px-3 flex rounded-md border border-reddit_border mx-4 flex-grow">
           <SearchIcon className="text-gray-300 h-6 w-6 mt-1" />
           <input type="text" className="bg-reddit_dark-brighter text-sm p-1 pl-2 pr-0 block focus:outline-none text-white" placeholder="Search" />
-        </form>
+        </form> */}
 
-        {user.username && (
+        {/* {user.username && (
           <>
             <button className="px-2 py-1">
               <ChatIcon className="text-gray-400 w-6 h-6 mx-2" />
@@ -51,17 +52,18 @@ function Header() {
               <PlusIcon className="text-gray-400 w-6 h-6 mx-2" />
             </button>
           </>
-        )}
-
+        )} */}
+        <div className = "flex relative w-3/4">
+        </div>
         {!user.username && (
-          <div className="mx-2 hidden sm:block">
+          <div className="mx-2  hidden sm:block">
             <Button outline={1} className="mr-1 h-8" onClick={() => authModal.setShow('login')}>Log In</Button>
             <Button className="h-8" onClick={() => authModal.setShow('register')}>Sign Up</Button>
           </div>
         )}
 
         <ClickOutHandler onClickOut={() => setUserDropdownVisibilityClass('hidden')}>
-          <button className="rounded-md flex ml-4 border border-gray-700" onClick={() => toggleUserDropdown()}>
+          <button className="absolute right-0 rounded-md flex ml-4 border border-gray-700" onClick={() => toggleUserDropdown()}>
             {!user.username && (
               <UserIcon className="w-6 h-6 text-gray-400 m-1" />
             )}
