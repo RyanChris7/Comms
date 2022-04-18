44dff62073c6ffe5e2b9235f4e95b2c57a16d797
HANSEN EMANUEL <hansenemanuel@hotmail.com>
2022-04-18 19:48:07 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -14,10 +14,10 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true,
+// }));
 
 app.use(VotingRoutes);
 
